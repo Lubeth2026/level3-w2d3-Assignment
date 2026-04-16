53023594ee672e9be3ea933432794986af64ef91
Lubeth <lubethjimenez@yahoo.com>
2026-04-15 21:46:58 -0400

    Add component 1 passing props to the page

--- appBuild/src/App.jsx
+++ appBuild/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Title from './components/Title'
 
 function App() {
 
   return (
     <>
-      <h1>Welcome</h1>
+      <h1>Welcome!!</h1>
+      <Title gameName="The Truth or Dare Game"/>
     </>
   )
 }
---
+++ appBuild/src/components/Title.jsx
@@ -0,0 +1,10 @@
+
+//Component #1 Title of the game//
+//Passes props through parent app.jsx//
+export default function Title({ gameName }){
+    return(
+        <>
+        <h1>{gameName}</h1>
+        </>
+    )
+}
