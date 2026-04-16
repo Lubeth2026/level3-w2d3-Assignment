312640a0c6bb3e55254b9d68f5153ad69444132b
Lubeth <lubethjimenez@yahoo.com>
2026-04-15 22:04:57 -0400

    Add component 2 passing props to the page

---
+++ appBuild/src/components/Description.jsx
@@ -0,0 +1,10 @@
+
+//Component #2 Description of the game//
+//Passes props through parent app.jsx//
+export default function Description({ message }){
+    return(
+        <>
+        <p>{message}</p>
+        </>
+    )
+}
